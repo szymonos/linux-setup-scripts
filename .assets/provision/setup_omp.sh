639b1b66ba2c5dc9cdd737b5a17f43fa02cab175
Szymon Osiecki <szymonos@outlook.com>
2022-10-30 23:45:46 +0100

    refactor: split setting bash and pwsh profile

--- .assets/provision/setup_omp.sh
+++ .assets/provision/setup_omp.sh
@@ -13,13 +13,3 @@ if [ -d /tmp/config/omp_cfg ]; then
   # clean config folder
   \rm -fr /tmp/config/omp_cfg
 fi
-
-# *add oh-my-posh invocation
-if ! grep -qw 'oh-my-posh' ~/.bashrc 2>/dev/null && type oh-my-posh &>/dev/null; then
-  cat <<EOF >>~/.bashrc
-# initialize oh-my-posh prompt
-if [ -f $OMP_PATH/theme.omp.json ] && type oh-my-posh &>/dev/null; then
-  eval "\$(oh-my-posh --init --shell bash --config $OMP_PATH/theme.omp.json)"
-fi
-EOF
-fi
