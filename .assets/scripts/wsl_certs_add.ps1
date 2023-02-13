1243daa7374e7e4db8916e3cd3675fb0485f2962
Szymon Osiecki <szymonos@outlook.com>
2023-02-13 20:56:54 +0100

    refactor(ps): wsl_certs_add

--- .assets/scripts/wsl_certs_add.ps1
+++ .assets/scripts/wsl_certs_add.ps1
@@ -5,18 +5,18 @@ Get certificates in chain and install them in the specified WSL distribution.
 .DESCRIPTION
 Script intercepts root and intermediate certificates in chain for specified sites
 and installs them in the specified WSL distro.
-It requires openssl application to be installed in Windows.
 
 .PARAMETER Distro
 Name of the WSL distro to install the certificate to.
-.PARAMETER Site [Optional]
-Site used for intercepting certificate chain.
+.PARAMETER Uri [Optional]
+Uri used for intercepting certificate chain.
 
 .EXAMPLE
 $Distro = 'Ubuntu'
-$Uri = 'www.powershellgallery.com'
-~install certificates in specified distro
+~install certificates into specified distro
 .assets/scripts/wsl_certs_add.ps1 $Distro
+~specify custom Uri
+$Uri = 'www.powershellgallery.com'
 .assets/scripts/wsl_certs_add.ps1 $Distro -u $Uri
 #>
 [CmdletBinding()]
@@ -65,7 +65,7 @@ begin {
 
     # create temp folder for saving certificates
     $tmpName = "tmp.$( -join ((0..9 + 'a'..'z') * 10 | Get-Random -Count 10))"
-    $tmp = New-Item -Path . -Name $tmpName -ItemType Directory
+    $tmpFolder = New-Item -Path . -Name $tmpName -ItemType Directory
 }
 
 process {
@@ -73,8 +73,12 @@ process {
     $tcpClient = [Net.Sockets.TcpClient]::new($Uri, 443)
     $sslStream = [Net.Security.SslStream]::new($tcpClient.GetStream())
 
-    $sslStream.AuthenticateAsClient($Uri)
-    $certificate = $sslStream.RemoteCertificate
+    try {
+        $sslStream.AuthenticateAsClient($Uri)
+        $certificate = $sslStream.RemoteCertificate
+    } finally {
+        $sslStream.Close()
+    }
 
     $chain = [Security.Cryptography.X509Certificates.X509Chain]::new()
     $isChainValid = $chain.Build($certificate)
@@ -88,25 +92,21 @@ process {
             $pem.AppendLine('-----BEGIN CERTIFICATE-----') | Out-Null
             $pem.AppendLine([System.Convert]::ToBase64String($certs[$i].RawData, 'InsertLineBreaks')) | Out-Null
             $pem.AppendLine('-----END CERTIFICATE-----') | Out-Null
-            # parse CN from Subject
+            # parse common name from the subject
             $cn = [regex]::Match($certs[$i].Subject, '(?<=CN=)(.)+?(?=,|$)').Value.Replace(' ', '_').Trim('"')
             # save PEM certificate
-            [IO.File]::WriteAllText([IO.Path]::Combine($tmp, "${cn}.crt"), $pem.ToString())
+            [IO.File]::WriteAllText([IO.Path]::Combine($tmpFolder, "${cn}.crt"), $pem.ToString())
             Write-Host "- ${cn}.crt"
         }
     } else {
-        Write-Error 'Error: SSL certificate chain validation failed'
+        Write-Error 'SSL certificate chain validation failed.'
     }
 
-    # move certificates to specified distro and install them
+    # copy certificates to specified distro and install them
     $cmd = "mkdir -p $($crt.path) && cp -f ${tmpName}/*.crt $($crt.path) && chmod 644 $($crt.path)/*.crt && $($crt.cmd)"
     wsl -d $Distro -u root --exec bash -c $cmd
 }
 
 end {
-    # close SslStream and TcpClient
-    $sslStream.Close()
-    $tcpClient.Close()
-    # remove temp folder
-    Remove-Item $tmp -Recurse
+    Remove-Item $tmpFolder -Recurse
 }
