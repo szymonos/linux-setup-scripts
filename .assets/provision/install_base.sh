4dafd0119b28ad17eeb1baa4000463c985954328
Szymon Osiecki <szymonos@outlook.com>
2023-04-25 23:48:18 +0200

    refactor(sh): install_base - install tracepath

--- .assets/provision/install_base.sh
+++ .assets/provision/install_base.sh
@@ -19,11 +19,11 @@ arch)
   ;;
 fedora)
   rpm -q patch &>/dev/null || dnf groupinstall -y 'Development Tools'
-  dnf install -qy bash-completion bind-utils curl dnf-plugins-core git jq redhat-lsb-core man-db openssl tar tree unzip vim
+  dnf install -qy bash-completion bind-utils curl dnf-plugins-core git iputils jq redhat-lsb-core man-db openssl tar tree unzip vim
   ;;
 debian | ubuntu)
   export DEBIAN_FRONTEND=noninteractive
-  apt-get update && apt-get install -y build-essential bash-completion ca-certificates gnupg dnsutils curl git jq lsb-release man-db openssl tar tree unzip vim
+  apt-get update && apt-get install -y build-essential bash-completion ca-certificates gnupg dnsutils curl git iputils-tracepath jq lsb-release man-db openssl tar tree unzip vim
   ;;
 opensuse)
   rpm -q patch &>/dev/null || zypper in -yt pattern devel_basis
