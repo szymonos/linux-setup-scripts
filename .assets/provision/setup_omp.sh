d5e1abf4aa0ed246700c103dc900f6ea6b0d879b
Szymon Osiecki <szymonos@outlook.com>
2023-05-03 19:02:07 +0200

    refactor: user context calculation

--- .assets/provision/setup_omp.sh
+++ .assets/provision/setup_omp.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 : '
 sudo .assets/provision/setup_omp.sh
-sudo .assets/provision/setup_omp.sh --theme "powerline"
+sudo .assets/provision/setup_omp.sh --theme nerd --user $(id -un)
 '
 if [ $EUID -ne 0 ]; then
-  echo -e '\e[91mRun the script as root!\e[0m'
+  printf '\e[31;1mRun the script as root.\e[0m\n'
   exit 1
 fi
 
 # parse named parameters
-theme=${1:-base}
+theme=${theme:-base}
+user=${user:-$(id -un 1000 2>/dev/null)}
 while [ $# -gt 0 ]; do
   if [[ $1 == *"--"* ]]; then
     param="${1/--/}"
@@ -18,8 +19,18 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+# check if specified user exists
+if ! grep -qw "^$user" /etc/passwd; then
+  if [ -n "$user" ]; then
+    printf "\e[31;1mUser does not exist ($user).\e[0m\n"
+  else
+    printf "\e[31;1mUser ID 1000 not found.\e[0m\n"
+  fi
+  exit 1
+fi
+
 # path variables
-CFG_PATH="/home/$(id -un 1000)/tmp/config/omp_cfg"
+CFG_PATH="$(sudo -u $user sh -c 'echo $HOME')/tmp/config/omp_cfg"
 OH_MY_POSH_PATH='/usr/local/share/oh-my-posh'
 # copy profile for WSL setup
 if [ -f .assets/config/omp_cfg/${theme}.omp.json ]; then
@@ -34,7 +45,7 @@ fi
 # *Copy oh-my-posh theme
 if [ -f $CFG_PATH/${theme}.omp.json ]; then
   mkdir -p $OH_MY_POSH_PATH
-  install -o root -g root -m 0644 $CFG_PATH/${theme}.omp.json $OH_MY_POSH_PATH/theme.omp.json
+  install -m 0644 $CFG_PATH/${theme}.omp.json $OH_MY_POSH_PATH/theme.omp.json
 fi
 
 # clean config folder
