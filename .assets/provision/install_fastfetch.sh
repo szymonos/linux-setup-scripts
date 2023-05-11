8edf4620bf5408d0d54db25dd594fb4b86e587be
Szymon Osiecki <szymonos@outlook.com>
2023-05-11 22:04:30 +0200

    feat(sh): install_fastfetch

---
+++ .assets/provision/install_fastfetch.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+: '
+sudo .assets/provision/install_fastfetch.sh >/dev/null
+'
+if [ $EUID -ne 0 ]; then
+  printf '\e[31;1mRun the script as root.\e[0m\n'
+  exit 1
+fi
+
+# determine system id
+SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=.*?\K(arch|fedora|debian|ubuntu|opensuse)' /etc/os-release)
+# check if package installed already using package manager
+APP='fastfetch'
+case $SYS_ID in
+alpine)
+  apk -e info $APP &>/dev/null && exit 0 || true
+  ;;
+arch)
+  pacman -Qqe $APP &>/dev/null && exit 0 || true
+  ;;
+fedora | opensuse)
+  rpm -q $APP &>/dev/null && exit 0 || true
+  ;;
+esac
+
+REL=$1
+retry_count=0
+# try 10 times to get latest release if not provided as a parameter
+while [ -z "$REL" ]; do
+  REL=$(curl -sk https://api.github.com/repos/LinusDierheimer/fastfetch/releases/latest | grep -Po '"tag_name": *"v?\K.*?(?=")')
+  ((retry_count++))
+  if [ $retry_count -eq 10 ]; then
+    printf "\e[33m$APP version couldn't be retrieved\e[0m\n" >&2
+    exit 0
+  fi
+  [[ -n "$REL" || $i -eq 10 ]] || echo 'retrying...' >&2
+done
+# return latest release
+echo $REL
+
+if type $APP &>/dev/null; then
+  VER=$($APP --version | grep -Po '(?<=\s)[0-9\.]+(?=\s)')
+  if [ "$REL" = "$VER" ]; then
+    printf "\e[32m$APP v$VER is already latest\e[0m\n" >&2
+    exit 0
+  fi
+fi
+
+printf "\e[92minstalling $APP v$REL\e[0m\n" >&2
+case $SYS_ID in
+arch)
+  if pacman -Qqe paru &>/dev/null; then
+    user=${1:-$(id -un 1000 2>/dev/null)}
+    if ! sudo -u $user true 2>/dev/null; then
+      if [ -n "$user" ]; then
+        printf "\e[31;1mUser does not exist ($user).\e[0m\n"
+      else
+        printf "\e[31;1mUser ID 1000 not found.\e[0m\n"
+      fi
+      exit 1
+    fi
+    sudo -u $user paru -Sy --needed --noconfirm $APP
+  else
+    printf '\e[33;1mWarning: paru not installed.\e[0m\n'
+  fi
+  ;;
+fedora)
+  dnf install -y $APP >&2 2>/dev/null || binary=true
+  ;;
+debian | ubuntu)
+  export DEBIAN_FRONTEND=noninteractive
+  while [[ ! -f $APP.deb && $retry_count -lt 10 ]]; do
+    curl -Lsk -o $APP.deb "https://github.com/LinusDierheimer/fastfetch/releases/download/${REL}/fastfetch-${REL}-Linux.deb"
+    ((retry_count++))
+  done
+  dpkg -i $APP.deb >&2 2>/dev/null && rm -f $APP.deb || binary=true
+  ;;
+opensuse)
+  zypper in -y $APP >&2 2>/dev/null || binary=true
+  ;;
+esac
