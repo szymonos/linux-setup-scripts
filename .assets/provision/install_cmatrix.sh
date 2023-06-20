769e2d983c5b897aa68bd9537d7c1706d62c1d46
Szymon Osiecki <szymonos@outlook.com>
2023-06-20 22:43:59 +0200

    feat(sh): install_cmatrix

---
+++ .assets/provision/install_cmatrix.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+: '
+sudo .assets/provision/install_cmatrix.sh >/dev/null
+'
+if [ $EUID -ne 0 ]; then
+  printf '\e[31;1mRun the script as root.\e[0m\n'
+  exit 1
+fi
+
+# determine system id
+SYS_ID="$(sed -En '/^ID.*(alpine|arch|fedora|debian|ubuntu|opensuse).*/{s//\1/;p;q}' /etc/os-release)"
+# check if package installed already using package manager
+APP='cmatrix'
+case $SYS_ID in
+alpine)
+  apk -e info $APP &>/dev/null && exit 0 || true
+  ;;
+arch)
+  pacman -Qqe $APP &>/dev/null && exit 0 || true
+  ;;
+fedora)
+  rpm -q $APP &>/dev/null && exit 0 || true
+  ;;
+opensuse)
+  exit 0
+  ;;
+debian | ubuntu)
+  dpkg -s $APP &>/dev/null && exit 0 || true
+  ;;
+esac
+
+printf "\e[92minstalling $APP" >&2
+case $SYS_ID in
+alpine)
+  apk add --no-cache $APP >&2 2>/dev/null
+  ;;
+arch)
+  pacman -Sy --needed --noconfirm --color=auto $APP >&2 2>/dev/null
+  ;;
+fedora)
+  dnf install -y $APP >&2 2>/dev/null
+  ;;
+debian | ubuntu)
+  apt-get update && apt-get install -y $APP >&2 2>/dev/null
+  ;;
+esac
