d53c7fb704e5ea5557ccd0f9e68524e0ea4981d0
Szymon Osiecki <szymonos@outlook.com>
2023-07-18 22:14:58 +0200

    refactor(sh): install_miniconda

--- .assets/provision/install_miniconda.sh
+++ .assets/provision/install_miniconda.sh
@@ -2,24 +2,23 @@
 : '
 .assets/provision/install_miniconda.sh
 '
-APP='conda'
-if [ -d $HOME/miniconda3 ]; then
-  printf "\e[32mDirectory already exists: '$HOME/miniconda3'\e[0m\n"
-  exit 0
-fi
+if type conda >/dev/null; then
+  conda update -n base -c defaults conda --yes
+  conda clean --yes --all
+else
+  printf "\e[92minstalling \e[1mminiconda\e[0m\n"
 
-printf "\e[92minstalling \e[1m$APP\e[0m\n"
+  retry_count=0
+  while [[ ! -f miniconda.sh && $retry_count -lt 10 ]]; do
+    curl -fsSLk -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    ((retry_count++))
+  done
+  bash ./miniconda.sh -b -p "$HOME/miniconda3" >/dev/null && rm ./miniconda.sh
 
-retry_count=0
-while [[ ! -f miniconda.sh && $retry_count -lt 10 ]]; do
-  curl -fsSLk -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-  ((retry_count++))
-done
-bash ./miniconda.sh -b -p "$HOME/miniconda3" >/dev/null && rm ./miniconda.sh
-
-# disable auto activation of the base conda environment
-"$HOME/miniconda3/bin/conda" config --set auto_activate_base false
-# disable conda env prompt if oh-my-posh is installed
-if [ -f /usr/bin/oh-my-posh ]; then
-  "$HOME/miniconda3/bin/conda" config --set changeps1 false
+  # disable auto activation of the base conda environment
+  "$HOME/miniconda3/bin/conda" config --set auto_activate_base false
+  # disable conda env prompt if oh-my-posh is installed
+  if [ -f /usr/bin/oh-my-posh ]; then
+    "$HOME/miniconda3/bin/conda" config --set changeps1 false
+  fi
 fi
