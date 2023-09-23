39d51ce1d9c96577f93eeb8536412aed09d2b864
Szymon Osiecki <szymonos@outlook.com>
2023-09-24 00:45:38 +0200

    feat(ps): wsl1_setup

--- wsl/wsl1_setup.ps1
+++ wsl/wsl1_setup.ps1
@@ -5,10 +5,7 @@ Setting up WSL distro(s).
 .DESCRIPTION
 You can use the script for:
 - installing base packages and setting up bash and pwsh shells,
-- installing docker-ce locally in WSL,
-- installing podman with distrobox,
 - installing tools for interacting with kubernetes,
-- setting gtk theme in WSLg,
 - installing Python environment management tools: venv and miniconda,
 - cloning GH repositories and setting up VSCode workspace,
 - updating packages in all existing WSL distros.
@@ -19,12 +16,7 @@ Name of the WSL distro to set up. If not specified, script will update all exist
 .PARAMETER Scope
 List of installation scopes. Valid values:
 - az: azure-cli if python scope specified, do-az from ps-modules if shell scope specified.
-- distrobox: podman and distrobox
-- docker: docker, containerd buildx docker-compose
-- k8s_base: kubectl, helm, minikube, k3d, k9s, yq
-- k8s_ext: flux, kubeseal, kustomize, argorollouts-cli
 - python: pip, venv, miniconda
-- rice: btop, cmatrix, cowsay, fastfetch
 - shell: bat, exa, oh-my-posh, pwsh, ripgrep
 Default: @('shell').
 .PARAMETER OmpTheme
@@ -32,36 +24,29 @@ Specify to install oh-my-posh prompt theme engine and name of the theme to be us
 You can specify one of the three included profiles: base, powerline, nerd,
 or use any theme available on the page: https://ohmyposh.dev/docs/themes/
 Default: 'base'
-.PARAMETER GtkTheme
-Specify gtk theme for wslg. Available values: light, dark.
-Default: automatically detects based on the system theme.
 .PARAMETER Repos
 List of GitHub repositories in format "Owner/RepoName" to clone into the WSL.
 .PARAMETER AddCertificate
 Intercept and add certificates from chain into selected distro.
-.PARAMETER FixNetwork
-Set network settings from the selected network interface in Windows.
 
 .EXAMPLE
 $Distro = 'Ubuntu'
 # :set up WSL distro using default values
-wsl/wsl_setup.ps1 $Distro
-wsl/wsl_setup.ps1 $Distro -AddCertificate
-wsl/wsl_setup.ps1 $Distro -FixNetwork -AddCertificate
+wsl/wsl1_setup.ps1 $Distro
+wsl/wsl1_setup.ps1 $Distro -AddCertificate
 # :set up WSL distro using specified values
 $Scope = @('az', 'python', 'shell')
-$Scope = @('az', 'docker', 'k8s_base', 'k8s_ext', 'python', 'shell')
-wsl/wsl_setup.ps1 $Distro -s $Scope
-wsl/wsl_setup.ps1 $Distro -s $Scope -AddCertificate
+wsl/wsl1_setup.ps1 $Distro -s $Scope
+wsl/wsl1_setup.ps1 $Distro -s $Scope -AddCertificate
 $OmpTheme = 'nerd'
-wsl/wsl_setup.ps1 $Distro -s $Scope -o $OmpTheme
-wsl/wsl_setup.ps1 $Distro -s $Scope -o $OmpTheme -AddCertificate
+wsl/wsl1_setup.ps1 $Distro -s $Scope -o $OmpTheme
+wsl/wsl1_setup.ps1 $Distro -s $Scope -o $OmpTheme -AddCertificate
 # :set up WSL distro and clone specified GitHub repositories
 $Repos = @('szymonos/linux-setup-scripts', 'szymonos/ps-modules')
-wsl/wsl_setup.ps1 $Distro -r $Repos -s $Scope -o $OmpTheme
-wsl/wsl_setup.ps1 $Distro -r $Repos -s $Scope -o $OmpTheme -AddCertificate
+wsl/wsl1_setup.ps1 $Distro -r $Repos -s $Scope -o $OmpTheme
+wsl/wsl1_setup.ps1 $Distro -r $Repos -s $Scope -o $OmpTheme -AddCertificate
 # :update all existing WSL distros
-wsl/wsl_setup.ps1
+wsl/wsl1_setup.ps1
 #>
 [CmdletBinding(DefaultParameterSetName = 'Update')]
 param (
@@ -71,7 +56,7 @@ param (
 
     [Parameter(ParameterSetName = 'Setup')]
     [Parameter(ParameterSetName = 'GitHub')]
-    [ValidateScript({ $_.ForEach({ $_ -in @('az', 'distrobox', 'docker', 'k8s_base', 'k8s_ext', 'oh_my_posh', 'python', 'rice', 'shell') }) -notcontains $false },
+    [ValidateScript({ $_.ForEach({ $_ -in @('az', 'oh_my_posh', 'python', 'shell') }) -notcontains $false },
         ErrorMessage = 'Wrong scope provided. Valid values: az distrobox docker k8s_base k8s_ext python rice shell')]
     [string[]]$Scope,
 
@@ -81,12 +66,6 @@ param (
     [ValidateNotNullOrEmpty()]
     [string]$OmpTheme,
 
-    [Parameter(ParameterSetName = 'Update')]
-    [Parameter(ParameterSetName = 'Setup')]
-    [Parameter(ParameterSetName = 'GitHub')]
-    [ValidateSet('light', 'dark')]
-    [string]$GtkTheme,
-
     [Parameter(Mandatory, ParameterSetName = 'GitHub')]
     [ValidateScript({ $_.ForEach({ $_ -match '^[\w-]+/[\w-]+$' }) -notcontains $false },
         ErrorMessage = 'Repos should be provided in "Owner/RepoName" format.')]
@@ -94,11 +73,7 @@ param (
 
     [Parameter(ParameterSetName = 'Setup')]
     [Parameter(ParameterSetName = 'GitHub')]
-    [switch]$AddCertificate,
-
-    [Parameter(ParameterSetName = 'Setup')]
-    [Parameter(ParameterSetName = 'GitHub')]
-    [switch]$FixNetwork
+    [switch]$AddCertificate
 )
 
 begin {
@@ -120,7 +95,7 @@ begin {
     # *get list of distros
     $lxss = Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss `
     | ForEach-Object { $_ | Get-ItemProperty } `
-    | Where-Object { $_.DistributionName -notmatch '^docker-desktop' -and $_.Flags -ge 8 } `
+    | Where-Object { $_.DistributionName -notmatch '^docker-desktop' -and $_.Flags -lt 8 } `
     | Select-Object DistributionName, DefaultUid, @{ N = 'Version'; E = { $_.Flags -lt 8 ? 1 : 2 } }
     if ($PsCmdlet.ParameterSetName -ne 'Update') {
         if ($Distro -in $lxss.DistributionName) {
@@ -135,14 +110,6 @@ begin {
         $lxss.Count ? '' : $null
     }
 
-    # determine GTK theme if not provided, based on system theme
-    if (-not $GtkTheme) {
-        $systemUsesLightTheme = Get-ItemPropertyValue -ErrorAction SilentlyContinue `
-            -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' `
-            -Name 'SystemUsesLightTheme'
-        $GtkTheme = $systemUsesLightTheme ? 'light' : 'dark'
-    }
-
     # set location to workspace folder
     Push-Location "$PSScriptRoot/.."
 }
@@ -153,16 +120,11 @@ process {
         # *perform distro checks
         $cmd = [string]::Join('',
             '[ -f /usr/bin/pwsh ] && shell="true" || shell="false";',
-            '[ -f /usr/bin/kubectl ] && k8s_base="true" || k8s_base="false";',
-            '[ -f /usr/bin/kustomize ] && k8s_ext="true" || k8s_ext="false";',
             '[ -f /usr/bin/oh-my-posh ] && omp="true" || omp="false";',
             '[ -d ~/.local/share/powershell/Modules/Az ] && az="true" || az="false";',
-            '[ -d /mnt/wslg ] && wslg="true" || wslg="false";',
             '[ -d "$HOME/miniconda3" ] && python="true" || python="false";',
-            'grep -qw "systemd.*true" /etc/wsl.conf 2>/dev/null && systemd="true" || systemd="false";',
-            'grep -Fqw "dark" /etc/profile.d/gtk_theme.sh 2>/dev/null && gtkd="true" || gtkd="false";',
-            'printf "{\"user\":\"$(id -un)\",\"shell\":$shell,\"k8s_base\":$k8s_base,\"k8s_ext\":$k8s_ext,',
-            '\"az\":$az,\"omp\":$omp,\"wslg\":$wslg,\"python\":$python,\"systemd\":$systemd,\"gtkd\":$gtkd}"'
+            'printf "{\"user\":\"$(id -un)\",\"shell\":$shell,',
+            '\"az\":$az,\"omp\":$omp,\"python\":$python}"'
         )
         # check existing packages
         $chk = wsl.exe -d $Distro --exec sh -c $cmd | ConvertFrom-Json -AsHashtable
@@ -172,8 +134,6 @@ process {
         # *determine scope if not provided
         if ($scopes.Count -eq 0) {
             switch ($chk) {
-                { $_.k8s_base } { $scopes.Add('k8s_base') | Out-Null }
-                { $_.k8s_ext } { $scopes.Add('k8s_ext') | Out-Null }
                 { $_.python } { $scopes.Add('python') | Out-Null }
                 { $_.shell } { $scopes.Add('shell') | Out-Null }
                 { $_.az } { $scopes.Add('az') | Out-Null }
@@ -187,11 +147,6 @@ process {
         Write-Host "$($Distro -eq $lxss.DistributionName[0] ? '': "`n")" -NoNewline
         # display distro name and installed scopes
         Write-Host "$Distro$($scopes ? " : `e[3m$scopes`e[23m" : '')" -ForegroundColor Magenta
-        # *fix WSL networking
-        if ($FixNetwork) {
-            Write-Host 'fixing network...' -ForegroundColor Cyan
-            wsl/wsl_network_fix.ps1 $Distro
-        }
         # *install certificates
         if ($AddCertificate) {
             Write-Host 'adding certificates in chain...' -ForegroundColor Cyan
@@ -207,41 +162,6 @@ process {
             exit
         }
         switch ($scopes) {
-            distrobox {
-                Write-Host 'installing distrobox...' -ForegroundColor Cyan
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_podman.sh
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_distrobox.sh $chk.user
-                continue
-            }
-            docker {
-                Write-Host 'installing docker...' -ForegroundColor Cyan
-                if (-not $chk.systemd) {
-                    # turn on systemd for docker autostart
-                    wsl/wsl_systemd.ps1 $Distro -Systemd 'true'
-                    wsl.exe --shutdown $Distro
-                }
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_docker.sh $chk.user
-                continue
-            }
-            k8s_base {
-                Write-Host 'installing kubernetes base packages...' -ForegroundColor Cyan
-                $rel_kubectl = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_kubectl.sh $Script:rel_kubectl && $($chk.k8s_base = $true)
-                $rel_kubelogin = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_kubelogin.sh $Script:rel_kubelogin
-                $rel_helm = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_helm.sh $Script:rel_helm
-                $rel_minikube = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_minikube.sh $Script:rel_minikube
-                $rel_k3d = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_k3d.sh $Script:rel_k3d
-                $rel_k9s = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_k9s.sh $Script:rel_k9s
-                $rel_yq = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_yq.sh $Script:rel_yq
-                continue
-            }
-            k8s_ext {
-                Write-Host 'installing kubernetes additional packages...' -ForegroundColor Cyan
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_flux.sh
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_kustomize.sh
-                $rel_kubeseal = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_kubeseal.sh $Script:rel_kubeseal
-                $rel_argoroll = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_argorolloutscli.sh $Script:rel_argoroll
-                continue
-            }
             oh_my_posh {
                 Write-Host 'installing oh-my-posh...' -ForegroundColor Cyan
                 $rel_omp = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_omp.sh $Script:rel_omp
@@ -257,14 +177,6 @@ process {
                 if ('az' -in $scopes) {
                     wsl.exe --distribution $Distro --exec .assets/provision/install_azurecli.sh --fix_certify true
                 }
-                continue
-            }
-            rice {
-                Write-Host 'ricing distro ...' -ForegroundColor Cyan
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_btop.sh
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_cmatrix.sh
-                wsl.exe --distribution $Distro --user root --exec .assets/provision/install_cowsay.sh
-                $rel_ff = wsl.exe --distribution $Distro --user root --exec .assets/provision/install_fastfetch.sh $Script:rel_ff
                 continue
             }
             shell {
@@ -313,25 +225,9 @@ process {
                 $modules.Add('do-az') | Out-Null
                 Write-Verbose "Added `e[3mdo-az`e[23m to be installed from ps-modules."
             }
-            if ($chk.k8s_base) {
-                $modules.Add('aliases-kubectl') | Out-Null
-                Write-Verbose "Added `e[3maliases-kubectl`e[23m to be installed from ps-modules."
-            }
             Write-Host "`e[3mCurrentUser`e[23m : $modules" -ForegroundColor DarkGreen
             $cmd = "@($($modules | Join-String -SingleQuote -Separator ',')) | ../ps-modules/module_manage.ps1 -CleanUp"
             wsl.exe --distribution $Distro --exec pwsh -nop -c $cmd
-        }
-        # *set gtk theme for wslg
-        if ($chk.wslg) {
-            $GTK_THEME = if ($GtkTheme -eq 'light') {
-                $chk.gtkd ? '"Adwaita"' : $null
-            } else {
-                $chk.gtkd ? $null : '"Adwaita:dark"'
-            }
-            if ($GTK_THEME) {
-                Write-Host "setting `e[3m$GtkTheme`e[23m gtk theme..." -ForegroundColor Cyan
-                wsl.exe --distribution $Distro --user root -- bash -c "echo 'export GTK_THEME=$GTK_THEME' >/etc/profile.d/gtk_theme.sh"
-            }
         }
     }
 
@@ -380,10 +276,6 @@ process {
         $builder.AppendLine('git config --global core.eol lf') | Out-Null
         $builder.AppendLine('git config --global core.autocrlf input') | Out-Null
         $builder.AppendLine('git config --global push.autoSetupRemote true') | Out-Null
-        if ($AddCertificate) {
-            # a guess, that if certs are being installed, you're behind MITM proxy without chunked transfer encoding
-            $builder.AppendLine('git config --global http.postBuffer 1048576000') | Out-Null
-        }
         wsl.exe --distribution $Distro --exec bash -c $builder.ToString().Trim()
 
         # *check ssh keys and create if necessary
