790224191dcb6169867cf624173e9a5a8b1f5e59
Szymon Osiecki <szymonos@outlook.com>
2024-09-14 15:50:02 +0200

    fix(ps): delete_ssh_config

--- .assets/trigger/delete_ssh_config.ps1
+++ .assets/trigger/delete_ssh_config.ps1
@@ -9,19 +9,22 @@ IP of the host in ssh config file.
 Name of the host in ssh config file.
 
 .EXAMPLE
-$IpAddress = '192.168.121.57'
+$IPAddress = '192.168.121.57'
 $HostName = 'vg-fedora-hv'
-.assets/trigger/delete_ssh_config.ps1 $IpAddress $HostName
+.assets/trigger/delete_ssh_config.ps1 -a $IPAddress -n $HostName
 #>
 [CmdletBinding()]
 param (
+    [Alias('a')]
     [Parameter(Mandatory, Position = 0)]
-    [string]$IpAddress,
+    [string]$IPAddress,
 
+    [Alias('n')]
     [Parameter(Mandatory, Position = 1)]
     [string]$HostName
 )
 
+# remove host entries from the .ssh/config file
 $sshConfig = [IO.Path]::Combine($HOME, '.ssh', 'config')
 if (Test-Path $sshConfig -PathType Leaf) {
     $content = [IO.File]::ReadAllText($sshConfig)
@@ -31,13 +34,19 @@ if (Test-Path $sshConfig -PathType Leaf) {
         [IO.File]::WriteAllText($sshConfig, $content.TrimEnd())
     }
 }
-Remove-Item ([IO.Path]::Combine($HOME, '.ssh', $HostName))
 
+# delete host dedicated ssh config file
+$sshHost = [IO.Path]::Combine($HOME, '.ssh', $HostName)
+if (Test-Path $sshHost -PathType Leaf) {
+    Remove-Item $sshHost
+}
+
+# remove entries from known_hosts for the specified IP address
 $knownHosts = [IO.Path]::Combine($HOME, '.ssh', 'known_hosts')
 if (Test-Path $knownHosts -PathType Leaf) {
     $content = [IO.File]::ReadAllLines($knownHosts)
-    if ($content | Select-String -Pattern "^$IpAddress") {
-        Write-Host "Removing '$IpAddress' fingerprint from ssh known_hosts..."
-        [IO.File]::WriteAllLines($knownHosts, ($content -notmatch "^$IpAddress"))
+    if ($content | Select-String -Pattern "^$IPAddress") {
+        Write-Host "Removing '$IPAddress' fingerprint from ssh known_hosts..."
+        [IO.File]::WriteAllLines($knownHosts, ($content -notmatch "^$IPAddress"))
     }
 }
