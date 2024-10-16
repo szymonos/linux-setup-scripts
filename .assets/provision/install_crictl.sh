8f2aa783e229e2ed15797074072d6f4095efaeee
Szymon Osiecki <szymonos@outlook.com>
2024-10-16 23:02:58 +0200

    feat(sh): crictl, etcdctl, nerdctl install scripts

--- .assets/provision/install_crictl.sh
+++ .assets/provision/install_crictl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 : '
-sudo .assets/provision/install_etcdctl.sh >/dev/null
+sudo .assets/provision/install_crictl.sh >/dev/null
 '
 if [ $EUID -ne 0 ]; then
   printf '\e[31;1mRun the script as root.\e[0m\n' >&2
@@ -11,12 +11,12 @@ fi
 . .assets/provision/source.sh
 
 # define variables
-APP='etcdctl'
+APP='crictl'
 REL=$1
 retry_count=0
 # get latest release if not provided as a parameter
 if [ -z "$REL" ]; then
-  if REL="$(get_gh_release_latest --owner 'etcd-io' --repo 'etcd')"; then
+  if REL="$(get_gh_release_latest --owner 'kubernetes-sigs' --repo 'cri-tools')"; then
     # return latest release
     echo $REL
   else
@@ -25,7 +25,7 @@ if [ -z "$REL" ]; then
 fi
 
 if type $APP &>/dev/null; then
-  VER=$(etcdctl version | sed -En 's/etcdctl version: ([0-9\.]+).*/\1/p')
+  VER=$(crictl --version | sed -En 's/crictl version v([0-9\.]+).*/\1/p')
   if [ "$REL" = "$VER" ]; then
     printf "\e[32m$APP v$VER is already latest\e[0m\n" >&2
     exit 0
@@ -36,13 +36,11 @@ printf "\e[92minstalling \e[1m$APP\e[22m v$REL\e[0m\n" >&2
 # create temporary dir for the downloaded binary
 TMP_DIR=$(mktemp -dp "$PWD")
 # calculate download uri
-URL="https://github.com/etcd-io/etcd/releases/download/v${REL}/etcd-v${REL}-linux-amd64.tar.gz"
+URL="https://github.com/kubernetes-sigs/cri-tools/releases/download/v${REL}/${APP}-v${REL}-linux-amd64.tar.gz"
 # download and install file
 if download_file --uri $URL --target_dir $TMP_DIR; then
-  tar -zxf "$TMP_DIR/$(basename $URL)" --strip-components=1 --no-same-owner -C "$TMP_DIR"
-  install -m 0755 "$TMP_DIR/etcd" /usr/local/bin/
-  install -m 0755 "$TMP_DIR/etcdctl" /usr/local/bin/
-  install -m 0755 "$TMP_DIR/etcdutl" /usr/local/bin/
+  tar -zxf "$TMP_DIR/$(basename $URL)" --no-same-owner -C "$TMP_DIR"
+  install -m 0755 "$TMP_DIR/crictl" /usr/local/bin/
 fi
 # remove temporary dir
 rm -fr "$TMP_DIR"
