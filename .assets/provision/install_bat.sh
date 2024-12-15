4a2f2d20767886e5dbf9375ecf5a40dc78fca5ee
Szymon Osiecki <szymonos@outlook.com>
2024-12-15 17:05:34 +0100

    refactor(sh): install_bat - batcat symlink

--- .assets/provision/install_bat.sh
+++ .assets/provision/install_bat.sh
@@ -60,16 +60,11 @@ fedora)
   ;;
 debian | ubuntu)
   export DEBIAN_FRONTEND=noninteractive
-  # create temporary dir for the downloaded binary
-  TMP_DIR=$(mktemp -dp "$PWD")
-  # calculate download uri
-  URL="https://github.com/sharkdp/bat/releases/download/v${REL}/bat_${REL}_amd64.deb"
-  # download and install file
-  if download_file --uri $URL --target_dir $TMP_DIR; then
-    dpkg -i "$TMP_DIR/$(basename $URL)" >&2 2>/dev/null || binary=true
+  apt-get update && apt-get install -y $APP
+  # create symlink for batcat
+  if [ -x /usr/bin/batcat ] && ! type bat &>/dev/null; then
+    ln -s /usr/bin/batcat /usr/bin/bat
   fi
-  # remove temporary dir
-  rm -fr "$TMP_DIR"
   ;;
 opensuse)
   zypper in -y $APP >&2 2>/dev/null || binary=true
