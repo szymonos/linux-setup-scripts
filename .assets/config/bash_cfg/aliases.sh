3fe0cdd4a62f9484a8a025042c8e4bc0849b9c5a
Szymon Osiecki <szymonos@outlook.com>
2025-05-23 21:26:42 +0200

    fix(sh): kubecolor_install

--- .assets/config/bash_cfg/aliases.sh
+++ .assets/config/bash_cfg/aliases.sh
@@ -124,6 +124,10 @@ if [ -x /usr/bin/kubens ]; then
   alias kn='kubens'
 fi
 
+if [ -x /usr/bin/kubecolor ]; then
+  alias kubectl='kubecolor'
+fi
+
 [ -x /usr/bin/rg ] && alias rg='rg --ignore-case' || true
 [ -x /usr/bin/fastfetch ] && alias ff='fastfetch' || true
 [ -x /usr/local/bin/tfswitch ] && alias tfswitch="tfswitch --bin='$HOME/.local/bin/terraform'" || true
