17e43134612b9f48812231436f539dd815abaed4
Szymon Osiecki <szymonos@outlook.com>
2025-11-28 07:41:30 +0100

    refactor(sh): install_gh
    Improved keyring upudate reliability

--- .assets/provision/install_gh.sh
+++ .assets/provision/install_gh.sh
@@ -7,10 +7,12 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
+# dotsource file with common functions
+. .assets/provision/source.sh
+
 # determine system id
 SYS_ID="$(sed -En '/^ID.*(alpine|arch|fedora|debian|ubuntu|opensuse).*/{s//\1/;p;q}' /etc/os-release)"
 # check if package installed already using package manager
-APP='gh'
 case $SYS_ID in
 alpine)
   apk -e info github-cli &>/dev/null && exit 0 || true
@@ -19,42 +21,20 @@ arch)
   pacman -Qqe github-cli &>/dev/null && exit 0 || true
   ;;
 fedora | opensuse)
-  rpm -q $APP &>/dev/null && exit 0 || true
+  rpm -q gh &>/dev/null && exit 0 || true
   ;;
 debian | ubuntu)
   export DEBIAN_FRONTEND=noninteractive
-  curl -fsSLk https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg 2>/dev/null
-  chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" >/etc/apt/sources.list.d/github-cli.list
-  dpkg -s $APP &>/dev/null && exit 0 || true
+  mkdir -p -m 755 /etc/apt/keyrings
+  if download_file --uri "https://cli.github.com/packages/githubcli-archive-keyring.gpg" --target_dir "/etc/apt/keyrings"; then
+    chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" >/etc/apt/sources.list.d/github-cli.list
+  fi
+  dpkg -s gh &>/dev/null && exit 0 || true
   ;;
 esac
 
-# dotsource file with common functions
-. .assets/provision/source.sh
-
-# define variables
-REL=$1
-# get latest release if not provided as a parameter
-if [ -z "$REL" ]; then
-  REL="$(get_gh_release_latest --owner 'cli' --repo 'cli')"
-  if [ -z "$REL" ]; then
-    printf "\e[31mFailed to get the latest version of $APP.\e[0m\n" >&2
-    exit 1
-  fi
-fi
-# return the release
-echo $REL
-
-if type $APP &>/dev/null; then
-  VER=$(gh --version | sed -En 's/.*\s([0-9\.]+)\s.*/\1/p')
-  if [ "$REL" = "$VER" ]; then
-    printf "\e[32m$APP v$VER is already latest\e[0m\n" >&2
-    exit 0
-  fi
-fi
-
-printf "\e[92minstalling \e[1m$APP\e[22m v$REL\e[0m\n" >&2
+printf "\e[92minstalling \e[1mgithub-cli\e[0m\n" >&2
 case $SYS_ID in
 alpine)
   apk add --no-cache github-cli >&2 2>/dev/null
