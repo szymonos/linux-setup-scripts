d7cca54892cb3f0f3db3971c4f4769d23e08ed65
Szymon Osiecki <szymonos@outlook.com>
2026-04-04 08:10:44 +0200

    feat(sh): install_humioctl

---
+++ .assets/provision/install_humioctl.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+: '
+sudo .assets/provision/install_humioctl.sh >/dev/null
+'
+set -euo pipefail
+
+if [ $EUID -ne 0 ]; then
+  printf '\e[31;1mRun the script as root.\e[0m\n' >&2
+  exit 1
+fi
+
+# dotsource file with common functions
+. .assets/provision/source.sh
+
+# define variables
+APP='humioctl'
+REL=${1:-}
+# get latest release if not provided as a parameter
+if [ -z "$REL" ]; then
+  REL="$(get_gh_release_latest --owner 'humio' --repo 'cli')"
+  if [ -z "$REL" ]; then
+    printf "\e[31mFailed to get the latest version of $APP.\e[0m\n" >&2
+    exit 1
+  fi
+fi
+# return the release
+echo $REL
+
+if type $APP &>/dev/null; then
+  VER=$($APP --version 2>&1 | sed -En 's/humioctl ([0-9\.]+) .*/\1/p')
+  if [ "$REL" = "$VER" ]; then
+    printf "\e[32m$APP v$VER is already latest\e[0m\n" >&2
+    exit 0
+  fi
+fi
+
+printf "\e[92minstalling \e[1m$APP\e[22m v$REL\e[0m\n" >&2
+# create temporary dir for the downloaded binary
+TMP_DIR=$(mktemp -d -p "$HOME")
+trap 'rm -fr "$TMP_DIR"' EXIT
+# calculate download uri
+URL="https://github.com/humio/cli/releases/download/v${REL}/humioctl_Linux_x86_64.tar.gz"
+# download and install file
+if download_file --uri "$URL" --target_dir "$TMP_DIR"; then
+  tar -zxf "$TMP_DIR/$(basename $URL)" -C "$TMP_DIR"
+  install -m 0755 "$TMP_DIR/humioctl" /usr/local/bin/
+fi
