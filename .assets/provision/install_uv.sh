6f8a90da2636cc90b90e9435eb4a1abacaf1a387
Szymon Osiecki <osiecki.s.1@pg.com>
2026-06-14 15:35:57 +0200

    fix(sh): install_uv - prefer UV_SYSTEM_CERTS / UV_NATIVE_TLS env vars

--- .assets/provision/install_uv.sh
+++ .assets/provision/install_uv.sh
@@ -38,7 +38,7 @@ if [ -x "$HOME/.local/bin/uv" ]; then
     retry_count=0
     max_retries=5
     while [ $retry_count -le $max_retries ]; do
-      $HOME/.local/bin/uv self update --system-certs >&2
+      UV_SYSTEM_CERTS=true UV_NATIVE_TLS=true "$HOME/.local/bin/uv" self update >&2
       [ $? -eq 0 ] && break || true
       ((retry_count++)) || true
       echo "retrying... $retry_count/$max_retries" >&2
